828c9f6345a1139b60d6e116a3908041c389cd34
Alvez <joaor4e@gmail.com>
2019-12-15 15:33:44 -0300

    project to create a navegation menu using javascript

---
+++ Project navegation-menu/script.js
@@ -0,0 +1,8 @@
+const area = document.getElementById("menu-area");
+function toogle(){
+    if (area.style.width == "0px") {
+        area.style.width = "200px";
+    }else{
+        area.style.width = "0px";
+    }
+};
