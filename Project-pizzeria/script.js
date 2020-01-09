c97a0546637a30dd4ed989484f72accee6012407
Alvez <joaor4e@gmail.com>
2020-01-09 11:05:55 -0300

    Fazendo aparecer os modals das pizzas

--- Project-pizzeria/script.js
+++ Project-pizzeria/script.js
@@ -6,6 +6,15 @@ pizzaJson.map((item,index)=>{
     pizzaitem.querySelector('.pizza-item--name').innerHTML= item.name;
     pizzaitem.querySelector('.pizza-item--desc').innerHTML= item.description;
     pizzaitem.querySelector('.pizza-item--price').innerHTML= `R$: ${item.price.toFixed(2)}`;
+    pizzaitem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        c('.pizzaWindowArea').style.opacity = 0;
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        },200);
+        c('.pizzaWindowArea').style.display = 'flex';
+
+    })
 
     c('.pizza-area').append( pizzaitem );
 });
