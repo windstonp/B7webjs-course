8e3a3251041a6846d23ceb9a91b412543c8aff07
Alvez <joaor4e@gmail.com>
2020-01-08 22:39:06 -0300

    adicionada informações das pizzas

--- Project-pizzeria/script.js
+++ Project-pizzeria/script.js
@@ -2,6 +2,10 @@ const c = (el)=>document.querySelector(el);
 const ca = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item,index)=>{
     let pizzaitem = c('.models .pizza-item').cloneNode(true);
+    pizzaitem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaitem.querySelector('.pizza-item--name').innerHTML= item.name;
+    pizzaitem.querySelector('.pizza-item--desc').innerHTML= item.description;
+    pizzaitem.querySelector('.pizza-item--price').innerHTML= `R$: ${item.price.toFixed(2)}`;
 
     c('.pizza-area').append( pizzaitem );
 });
